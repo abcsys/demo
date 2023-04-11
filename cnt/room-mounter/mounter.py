171e34c518874f6b685aa3b9c2edf3fa11ab1cce
Jamsheed Mistri <jmistri@berkeley.edu>
2023-04-11 15:57:34 -0700

    Add room occupancy data based on BSSID

--- cnt/room-mounter/mounter.py
+++ cnt/room-mounter/mounter.py
@@ -2,7 +2,7 @@ import subprocess
 import json
 import sys
 
-DIGIPHONE_NAME = "jamsheediphone"
+DIGIPHONE_NAMES = ["jamsheediphone", "silveryiphone"]
 
 # Read rooms.json file for room data
 with open('rooms.json') as file:
@@ -16,58 +16,59 @@ printed = False
 
 # Loop forever to mount
 while True:
-    output = subprocess.run(f'digi query -f json {DIGIPHONE_NAME}@bssid "sort -r event_ts | head"', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
+    for digiphone_name in DIGIPHONE_NAMES:
+        output = subprocess.run(f'digi query -f json {digiphone_name}@bssid "sort -r event_ts | head"', shell=True, stdout=subprocess.PIPE).stdout.decode('utf-8')
 
-    if output == "":
-        if not printed:
-            print(f'{DIGIPHONE_NAME} is not currently in any known rooms')
-            printed = True
-        continue
+        if output == "":
+            if not printed:
+                print(f'{digiphone_name} is not currently in any known rooms')
+                printed = True
+            continue
 
-    try:
-        curr_bssid = json.loads(str(output))['bssid']
-    except json.decoder.JSONDecodeError:
-        continue
+        try:
+            curr_bssid = json.loads(str(output))['bssid']
+        except json.decoder.JSONDecodeError:
+            continue
 
-    found_room = False
+        found_room = False
 
-    # Search through our list of rooms
-    for room in rooms:
-        room_name, room_bssid, room_digi = room.values()
+        # Search through our list of rooms
+        for room in rooms:
+            room_name, room_bssid, room_digi = room.values()
 
-        if curr_bssid == mounted_bssid:
-            found_room = True
-            break
+            if curr_bssid == mounted_bssid:
+                found_room = True
+                break
 
-        # If we found a match, and it's not the same as the currently mounted room
-        elif curr_bssid == room_bssid:
-            # Unmount & print info
-            if mounted_digi is None:
-                print(f'{DIGIPHONE_NAME} connected to {room_name}')
-            else:
-                subprocess.run(f'digi space mount -d {DIGIPHONE_NAME} {mounted_digi}', shell=True)
-                print(f'{DIGIPHONE_NAME} moved from {mounted_name} to {room_name}')
+            # If we found a match, and it's not the same as the currently mounted room
+            elif curr_bssid == room_bssid:
+                # Unmount & print info
+                if mounted_digi is None:
+                    print(f'{digiphone_name} connected to {room_name}')
+                else:
+                    subprocess.run(f'digi space mount -d {digiphone_name} {mounted_digi}', shell=True)
+                    print(f'{digiphone_name} moved from {mounted_name} to {room_name}')
 
-            # Mount
-            subprocess.run(f'digi space mount {DIGIPHONE_NAME} {room_digi}', shell=True)
+                # Mount
+                subprocess.run(f'digi space mount {digiphone_name} {room_digi}', shell=True)
 
-            # Update state
-            mounted_name, mounted_bssid, mounted_digi = room_name, room_bssid, room_digi
-            printed = False
+                # Update state
+                mounted_name, mounted_bssid, mounted_digi = room_name, room_bssid, room_digi
+                printed = False
 
-            # Stop searching
-            found_room = True
-            break
+                # Stop searching
+                found_room = True
+                break
 
-    if not found_room:
-        if mounted_digi is not None:
-            subprocess.run(f'digi space mount -d {DIGIPHONE_NAME} {mounted_digi}', shell=True)
+        if not found_room:
+            if mounted_digi is not None:
+                subprocess.run(f'digi space mount -d {digiphone_name} {mounted_digi}', shell=True)
 
-        # If we reach here, we are not in any currently known rooms
-        mounted_name = None
-        mounted_bssid = None
-        mounted_digi = None
+            # If we reach here, we are not in any currently known rooms
+            mounted_name = None
+            mounted_bssid = None
+            mounted_digi = None
 
-        if not printed:
-            print(f'{DIGIPHONE_NAME} is not currently in any known rooms')
-            printed = True
+            if not printed:
+                print(f'{digiphone_name} is not currently in any known rooms')
+                printed = True
